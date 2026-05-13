3f61262d7d0c81397309c5c571fb17aced0e5e21
Wathisa <wathisa@gmail.com>
2026-05-13 15:08:43 +0700

    backend

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    username: "Wathisa",
+    email: "wathisa@example.com",
+    password: "pass123",
+  },
+  {
+    id: "2",
+    username: "DinDin",
+    email: "dindin@example.com",
+    password: "pass123",
+  },
+  {
+    id: "3",
+    username: "GongYoo",
+    email: "gongyoo@example.com",
+    password: "pass123",
+  },
+];
